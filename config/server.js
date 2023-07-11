d76e032102279694698d2b71ceb5266ed95d6f03
Huriel Becker <becker.programador@outlook.com>
2023-07-11 09:35:17 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7ad8240eacf5149d4886174f08971f7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7fa0641d-df0d-47f8-ae7f-e189c1fcc46b'
+};
